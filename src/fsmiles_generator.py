b5e0b1fd9d2ea4bda91f8ce8701785cffb4de701
cdallas8 <92080785+cdallas8@users.noreply.github.com>
2024-03-21 16:40:06 +0100

    format fsmiles generator

--- src/fsmiles_generator.py
+++ src/fsmiles_generator.py
@@ -22,20 +22,15 @@ molecule = parse_sdf(mol_path)[0]
 
 # Find the cuttable bonds
 cuttable_bonds = find_cuttable_bonds(molecule)
-print(cuttable_bonds)
 
-# Find bonds from rings 
+# Find bonds belonging to rings & merge 
 ring_bonds = get_ring_bond_indexes(molecule)
-print(ring_bonds)
-
 ring_bonds = merge_bonds(ring_bonds)
-print(ring_bonds)
 
-# Remove ring bonds from cuttable bonds 
+# Filter out ring bonds from cuttable bonds 
 bonds =  filter_bonds(cuttable_bonds, ring_bonds)
-print(bonds)
 
-# Cut the identified bonds
+# Cut the identified bonds & visualize fragments 
 fragments = cut_bonds(molecule, bonds)
 
 print("Number of fragments:", len(fragments))
@@ -43,14 +38,13 @@ print("Number of fragments:", len(fragments))
 for i, fragment in enumerate(fragments):
     print(f"Fragment {i+1} SMILES:", Chem.MolToSmiles(fragment))
 
+# Draw fragments 
+draw_mol(fragments, draw_path)
+
 # Write fragment SMILES to file 
 fragments_file(fragments, smiles_out)
 
-# Draw the fragments
-draw_mol(fragments, draw_path)
-
-# Generate FSMILES
+# Generate FSMILES & write to .txt file 
 fsmiles = generate_fsmiles(fragments, fsmiles_out)
-print(fsmiles)
 
 ##############################################################################
